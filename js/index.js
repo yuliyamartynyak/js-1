289a80f9528e7d57614c6ad570b1e5c8d6fda64b
Yuliya Martynyak <yuliya.martynyak@gmail.com>
2019-02-01 23:04:59 +0000

    on fire

---
+++ js/index.js
@@ -0,0 +1,14 @@
+//let greetings = 10+15;
+//alert(greetings);
+
+//const name = prompt('Enter your name');
+//const msg = 'Hello, ' + name;
+//alert(msg);
+
+//const age = +prompt ('Enter your age');
+//const newAge = age + 1;
+//alert(newAge);
+
+const age = parseInt(prompt('Enter your age'));
+const newAge = age + 1;
+alert(newAge);
