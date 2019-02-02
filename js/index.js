8fa0e4daf7aa6f5cb396bdf8cffdacc8653cf47a
Yuliya Martynyak <yuliya.martynyak@gmail.com>
2019-02-02 14:24:17 +0000

    On fire

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-
 const a = parseFloat(prompt('Enter a'));
 const b = parseFloat(prompt('Enter b'));
 const c = parseFloat(prompt('Enter c'));
@@ -9,30 +8,21 @@ alert(result);
 function solveQuardEq(a, b, c) {
     const diskr = calcDisk(a, b, c);
     if(diskr > 0) {
-      let x1 = (-b + Math.sqrt(diskr))/(2*a);
-      let x2 = (-b - Math.sqrt(diskr))/(2*a);
-      return x1, x2;
+      const x1 = (-b + Math.sqrt(diskr))/(2*a);
+      const x2 = (-b - Math.sqrt(diskr))/(2*a);
+      return 'x1 ' + x1 + 'x2 ' + x2;
     } else if (diskr === 0) {
-        let x = - (b / (2*a));
+        const x = - (b / (2*a));
         return x;
-    } else if (diskr < 0) {
-        let x = null;
-        return x;
-            
-        }
-}
+    }
+    return 'no result'
+    
+    }
+
 
 function calcDisk(a, b, c) {
 return b*b - 4*a*c;    
 }
-
-
-
-
-
-
-
-
 
 
 
@@ -69,13 +59,5 @@ return b*b - 4*a*c;
      //   return 'adult';
     //}
 //}
-
-
-
-
-
-
-
-
 
 
